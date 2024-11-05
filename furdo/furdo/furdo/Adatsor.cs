b8af57e151471a903e639931a9ac7c0463539643
Mazskoasztar <144772884+mazskoasztar@users.noreply.github.com>
2024-11-05 14:02:18 +0100

    Add files via upload

---
+++ furdo/furdo/furdo/Adatsor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace furdo
+{
+    internal class Adatsor
+    {
+        
+            public int vendegazonosito { get; set; }
+            public int furdoreszleg { get; set; }
+            public bool kibelepo { get; set; }
+            public int ora { get; set; }
+            public int perc { get; set; }
+            public int masodperc { get; set; }
+
+            public Adatsor(string sor)
+            {
+                string[] sz = sor.Split(' ');
+                this.vendegazonosito = int.Parse(sz[0]);
+                this.furdoreszleg = int.Parse(sz[1]);
+                if (int.Parse(sz[2]) == 0)
+                {
+                    kibelepo = true;
+                }
+                this.ora= int.Parse(sz[3]);
+                this.perc = int.Parse(sz[4]);
+                this.masodperc = int.Parse(sz[5]);
+            }
+        
+    }
+}
---
+++ furdo/furdo/furdo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace furdo
+{
+    
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fajl = File.ReadAllLines("furdoadat.txt",Encoding.UTF8);
+            List<Adatsor> listaFurdo = new List<Adatsor>();
+            foreach (var item in fajl)
+            {
+                listaFurdo.Add(new Adatsor(item));
+            }
+
+            var feladat2 = listaFurdo.Where(x => x.kibelepo == false && x.furdoreszleg == 0).ToList();
+
+            Console.WriteLine($"2. feladat: Az első vendég{feladat2[0].ora}:{feladat2}:{feladat2}");
+        }
+    }
+}
